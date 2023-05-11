88ddcb25d6bda08e548a2d7f89b4bf8e99016a53
Ulia0711 <uliaesina94@gmail.com>
2023-05-12 00:41:32 +0600

    ORM в Django и работа с формами. Часть 2. ДЗ

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
-from .views import index, about, post_single
+from rest_framework.routers import DefaultRouter
+from .views import index, about, post_single, post_form, PostViewSet
 
-urlpatterns = [
-    path('', index, name='index'),
-    path('<int:pk>/', post_single, name='single'),
-    path('about', about, name='about'),
+router=DefaultRouter()
+router.register('post',PostViewSet,basename='post')
+
+
+urlpatterns=[
+    path('',index,name='index'),
+    path('<int:pk>/',post_single,name='single'),
+    path('post/new/',post_form,name="post_form"),
+    path('about/',about,name='about'),
+    path('api/post/list/',PostViewSet.as_view({'get':'list'})),
+    path('api/',include(router.urls)),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post, AboutUs
+from blog.forms import PostForm
+from django.shortcuts import redirect
+from django.utils import timezone
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework import mixins
+from .serializers import PostSerializer
+from .models import Post
+
+class PostViewSet(GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin):
+    serializer_class=PostSerializer
+    queryset=Post.objects.all()
 
 def index(request):
-    news=Post.objects.all()
-    return render(request,'index.html',{'posts':news})
+    news = Post.objects.all()
+    return render(request, 'index.html', {'posts': news})
 
 def about(request):
     about_us = AboutUs.objects.all()
-    return render(request,'about.html', {'about_us': about_us})
+    return render(request, 'about.html', {'about_us': about_us})
 
 def post_single(request, pk):
-    p=get_object_or_404(Post.objects.all(),pk=pk)
-    return render(request, 'post_single.html', {'post':p})
-    # Post.objects.get(pk=pk)
+    p = get_object_or_404(Post.objects.all(), pk=pk)
+    return render(request, 'post_single.html', {'post': p})
+
+def post_form(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('single', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post_add.html', {'form': form})
